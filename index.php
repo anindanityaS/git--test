88243c18341ee8677f835fbe45c2e490deb3d4b8
anindanityaS <anindanitya@gmail.com>
2020-10-05 10:28:44 +0700

    menambah list

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
     <h1>Hello World</h1>
     <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab, voluptates porro accusamus sed nostrum non iure velit enim tenetur quae. Laborum laboriosam maxime dolores accusamus, unde aliquid totam nostrum vitae?
     </p>
+    <ul>
+    <li>satu</li>
+    <li>dua</li>
+
+    </ul>
 </body>
 </html>
